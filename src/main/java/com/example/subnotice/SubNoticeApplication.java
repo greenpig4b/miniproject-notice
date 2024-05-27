4db2c27e7318745db0a062aa01dd7763ad78e7f4
kimjihun <greenpig4b@naver.com>
2024-05-27 17:05:13 +0900

    초기세팅

---
+++ src/main/java/com/example/subnotice/SubNoticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.subnotice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubNoticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubNoticeApplication.class, args);
+    }
+
+}
